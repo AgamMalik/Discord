d7cce41a1165225827346de22bb75b48d39e6392
AgamMalik <agam.malik31@gmail.com>
2023-09-16 17:55:57 +0530

    next commit

--- discord-frontend/src/Dashboard/FriendsSideBar/PendingInvitationsList/PendingInvitationsList.js
+++ discord-frontend/src/Dashboard/FriendsSideBar/PendingInvitationsList/PendingInvitationsList.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import { styled } from "@mui/system";
+import PendingInvitationsListItem from './PendingInvitationsListItem';
+
+
+const DUMMY_INVITATIONS = [
+  {
+    _id: "1",
+    senderId: {
+      username: "Mark",
+      mail: "dummy@ad.com",
+    },
+  },
+  {
+    _id: "2",
+    senderId: {
+      username: "John",
+      mail: "john@ad.com",
+    },
+  },
+];
 
 const MainContainer = styled("div")({
-  width: "10%",
+  width: "100%",
   height: "22%",
   display: "flex",
   flexDirection: "column",
@@ -14,7 +33,14 @@ const MainContainer = styled("div")({
 const PendingInvitationsList = () => {
   return (
     <MainContainer>
-
+      {DUMMY_INVITATIONS.map(invitation => (
+        <PendingInvitationsListItem
+        key={invitation._id}
+        id={invitation._id}
+        username={invitation.senderId.username}
+        mail= {invitation.senderId.mail}
+        />
+      ))}
     </MainContainer>
   )
 }
